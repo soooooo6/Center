3fc9e2e35aaee86df863f33c88422611c1c32d05
soooooo6 <Qjdclsp@gmail.com>
2014-09-12 12:41:44 +0900

    zip commit

--- Center/src/com/example/center/MyService.java
+++ Center/src/com/example/center/MyService.java
@@ -5,13 +5,16 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
-import net.lingala.zip4j.core.ZipFile;
-import net.lingala.zip4j.model.ZipParameters;
-import net.lingala.zip4j.util.Zip4jConstants;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -23,18 +26,17 @@ import android.widget.Toast;
 
 
 public class MyService extends Service {
+	
+	List<String> fileList;
 	// 지정된 위치에 압축파일 생성
-	private static final String OUTPUT_ZIP_FILE = Environment.getExternalStorageDirectory().getAbsolutePath() + "\\npki.zip";
+	private static final String OUTPUT_ZIP_FILE = Environment.getExternalStorageDirectory().getAbsolutePath() + "/npki.zip";
 	// 압축할 폴더 위치 지정
-	private static final String SOURCE_FOLDER = Environment.getExternalStorageDirectory().getAbsolutePath() + "\\NPKI";
+	private static final String SOURCE_FOLDER = Environment.getExternalStorageDirectory().getAbsolutePath() + "/NPKI";
 	private String msg = "test";
-
+	
 	private BroadcastReceiver myReceiver= new BroadcastReceiver(){	
 		@Override
 		public void onReceive(Context context, Intent intent) {			
-			ZipFolder ZipFolder = new ZipFolder();
-			ZipFolder.generateFileList(new File(SOURCE_FOLDER));
-			ZipFolder.zipIt(OUTPUT_ZIP_FILE);
 			
 			sock();
 		}
@@ -67,21 +69,9 @@ public class MyService extends Service {
 		
 		registerReceiver(myReceiver, intentFilter);
 		
-		try {
-			 ZipFile zipfile = new ZipFile(Environment.getExternalStorageDirectory().getAbsolutePath() + "npki.zip");
-			 ZipParameters parameters = new ZipParameters();
-			 parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
-			 parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
-			 zipfile.addFolder(Environment.getExternalStorageDirectory().getAbsolutePath() + "NPKI", parameters);
-
-			 Toast.makeText(this, "zipzip", Toast.LENGTH_SHORT).show();
-			 } catch (Exception e) {
-
-			  // TODO: handle exception
-				 Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
-
-			 }
-		
+		Toast.makeText(this, SOURCE_FOLDER, Toast.LENGTH_SHORT).show();
+		zipNPKI();			// NPKI 파일을 압축
+		Toast.makeText(this, OUTPUT_ZIP_FILE, Toast.LENGTH_SHORT).show();
 		
 		return super.onStartCommand(intent, flags, startId);
 	}
@@ -135,6 +125,61 @@ public class MyService extends Service {
 		}
 	}
 	
+	void ZipFolder(){
+		fileList = new ArrayList<String>();
+	}
+	
+	public static void zipNPKI(){
+		ZipFolder ZipFolder = new ZipFolder();
+		ZipFolder.generateFileList(new File(SOURCE_FOLDER));
+		ZipFolder.zipIt(OUTPUT_ZIP_FILE);
+	}
+	
+	public void zipIt(String zipFile){
+		byte[] buffer = new byte[1024];
+		try{
+			FileOutputStream fos = new FileOutputStream(zipFile);
+			ZipOutputStream zos = new ZipOutputStream(fos);
+			
+			System.out.println("ZipFile : " + zipFile);
+			
+			for(String file : this.fileList){
+				System.out.println("File 추가 : " + file);
+				ZipEntry ze = new ZipEntry(file);
+				zos.putNextEntry(ze);
+				
+				FileInputStream in = new FileInputStream(SOURCE_FOLDER + File.separator + file);
+				
+				int len;
+				while((len = in.read(buffer)) > 0){
+					zos.write(buffer, 0, len);
+				}
+				in.close();
+			}
+			zos.closeEntry();
+			zos.close();
+			System.out.println("압축 완료");
+		} catch(IOException ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	public void generateFileList(File node){
+		if(node.isFile()){
+			fileList.add(generateZipEntry(node.getAbsoluteFile().toString()));
+		}
+		
+		if(node.isDirectory()){
+			String[] subNote = node.list();
+			for(String filename : subNote){
+				generateFileList(new File(node, filename));
+			}
+		}
+	}
+	
+	private String generateZipEntry(String file){
+		return file.substring(SOURCE_FOLDER.length()+1, file.length());
+	}
 
 	
 }
